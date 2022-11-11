a2f7020830816f858f643029bf8c0dd247d2216a
Yoonseo Han <yooncer00@gmail.com>
2022-11-12 01:55:27 +0800

    Set up discord bot

--- NBA_Stats.js
+++ NBA_Stats.js
@@ -5,15 +5,14 @@ const TOKEN = config.chatbot_token;
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.on('ready', () => {
+client.on("ready", () =>{
+    console.log("The AI bot is online"); //message when bot is online
     console.log(`Logged in as ${client.user.tag}!`);
 });
-
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isChatInputCommand()) return;
-
-    if (interaction.commandName === 'ping') {
-        await interaction.reply('Pong!');
+client.on("message", (message) => {
+    message.channel.send("HELLO TEST");
+    if (message.content.substring(0, 1) === "!") {
+        message.channel.send("Hello from AI bot"); //reply if message has "!" as first character
     }
 });
 client.login(TOKEN);
